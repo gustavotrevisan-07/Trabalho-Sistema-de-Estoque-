ca75d363f8aedc85e0fe57fb9f1d06b0a7d0b0c0
Gustavo Trevisan <gustavotrevisan791@gmail.com>
2026-09-06 16:49:45 -0300

    Remoção por código dos itens

--- projeto_exemplo/estruturas/fila.py
+++ projeto_exemplo/estruturas/fila.py
@@ -17,6 +17,11 @@ class Fila:
 
         return self._lista_de_valores[0]
 
+    def remover_por_codigo(self, codigo):
+        for indice, item in enumerate(self._lista_de_valores):
+            if item.codigo == codigo:
+                return self._lista_de_valores(indice)
+
     def is_empty(self):
         return len(self._lista_de_valores) == 0
 
